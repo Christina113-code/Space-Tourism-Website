af6208d18f03217649166e5b1a23d42cd0386481
Christina <christinajun113@gmail.com>
2022-10-03 14:10:14 -0400

    basic homepg styles

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,13 +3,10 @@ import Page from './Page'
 import '../App.css';
 const Home = () => {
   return (
-    <>
-     <Page subtitle= {'So, you want to travel to'} title={<h1 >Space</h1>} description={"Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!"} />
-<div className="page__white-circle">
-    
-    <p className = "page__white-circle__p">Explore</p>
-  </div>
-    </>
+    <div>
+     <Page homepage = {true} subtitle= {<h2>So, you want to travel to</h2>} title={<h1 >Space</h1>} description={"Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!"} />
+
+    </div>
  
   )
 }
--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,17 +1,23 @@
 import React from "react";
 
-const Page = ({ img, menu, header, title, subtitle, description, bg_img }) => {
+const Page = ({ homepage, img, menu, header, title, subtitle, description, bg_img }) => {
   return (
     // bg-img later!!
-    <div className="h-screen bg-black">
+    <div className="h-screen bg-black ">
      
-      <h1 className="text-gray">{header}</h1>
+      <h1 className="text-white pt-8">{header}</h1>
       <img src={img} alt="" />
       <div>{menu}</div>
       
-      <subtitle>{subtitle}</subtitle>
-      <h1 >{title}</h1>
-      <p>{description}</p>
+      <subtitle className="uppercase text-light-gray font-condensed font-base text-center">{subtitle}</subtitle>
+      <h1 className="text-white text-[80px] font-bellefair uppercase text-center ">{title}</h1>
+      <p className="text-light-gray font-barlow text-base text-center pb-4 px-8">{description}</p>
+      {homepage? 
+      <div className="circle | h-36 w-36 bg-white rounded-full flex items-center justify-center mt-16 mx-auto">
+    
+      <p className = "uppercase font-bellefair text-lg">Explore</p>
+    </div>: ''
+    }
     </div>
   );
 };
